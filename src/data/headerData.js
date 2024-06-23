323be3fae1e71e26de338d52b782cb1691e24f53
Rajeswaran <rajeswaran149@gmail.com>
2024-06-23 10:41:51 +0530

    update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -9,5 +9,6 @@ export const headerData = {
   imagebw: "",
   resumePdf:
     // "https://drive.google.com/file/d/1dvPNfpLfs7GbDa5p7PfiZfFHCSx-kcs8/view?usp=sharing",
-    "https://drive.google.com/file/d/10-ZD3Jdfd9esELRFuU34ShvdwGQtG9U2/view?usp=drive_link"
+    // "https://drive.google.com/file/d/10-ZD3Jdfd9esELRFuU34ShvdwGQtG9U2/view?usp=drive_link",
+    "https://drive.google.com/file/d/1g8jAADGVlj-VY6UZSr0EKSd9YrkExt1G/view?usp=drive_link"
 };
