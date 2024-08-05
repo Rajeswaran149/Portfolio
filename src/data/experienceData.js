75da3414391a884598df7da0529a2aab38d160b2
Rajeswaran <rajeswaran149@gmail.com>
2024-08-05 11:08:36 +0530

    update "experience"

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,20 +1,27 @@
 export const experienceData = [
   {
     id: 1,
+    company: "The Tann Mann Gaadi ",
+    jobtitle: "MERN Stack Developer",
+    startYear: "July 2024",
+    endYear: "present",
+  },
+  {
+    id: 2,
     company: "Acelion Energy Storage Solutions pvt ltd",
     jobtitle: "Jr.Full Stack Developer",
     startYear: "June 2023",
     endYear: "Jan 2024",
   },
   {
-    id: 2,
+    id: 3,
     company: "EDR Continous Information pvt ltd",
     jobtitle: "Lidar Analyst",
     startYear: "Jan 2018",
     endYear: "dec 2020",
   },
   {
-    id: 3,
+    id: 4,
     company: "Balaji Industries",
     jobtitle: "Graduate Engineering Trainee",
     startYear: "July 2016",
