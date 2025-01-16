8add9661d5fb6becb1bb910ccbe1944a8ce1e6d2
Rajeswaran <rajeswaran149@gmail.com>
2025-01-16 13:05:27 +0530

    update resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,11 +4,12 @@ export const headerData = {
   name: "RAJESWARAN",
   title: "Hello! I am",
   desciption:
-    "Hello! I am RAJESWARAN, A passionate programmer a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
+    "Hello! I am a full-stack developer with 3 years of experience. I love exploring new technologies and solving problems.",
   image: Rajes,
   imagebw: "",
   resumePdf:
     // "https://drive.google.com/file/d/1dvPNfpLfs7GbDa5p7PfiZfFHCSx-kcs8/view?usp=sharing",
       //  "https://drive.google.com/file/d/1zJBXW-uTTSJ59izp-_p2SCHeDeesBIiM/view?usp=sharing",
-        "https://drive.google.com/file/d/1zU00KbA2KVGvdh5quPf1Y9wVvztCp1AY/view?usp=sharing"
+        // "https://drive.google.com/file/d/1zU00KbA2KVGvdh5quPf1Y9wVvztCp1AY/view?usp=sharing",
+        "https://drive.google.com/file/d/1u9Ms0R4z3WHSXDafgJTDGjWF32GcFyZW/view?usp=sharing"
 };
